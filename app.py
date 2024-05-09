f10b7d755076bd819490cfbfccf01639e7725b7b
Kelly Ramos <kramos@fcc.org.br>
2024-05-09 08:55:40 -0300

    cors configurado

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def usuarios():
     return "Ola usuarios"

9e4106f50276445a8bf2cfa35c9c087e5d0c2f38
Kelly Ramos <kramos@fcc.org.br>
2024-05-09 10:02:34 -0300

    arquivo json adicionado a rota 'dados'

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, json, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-def usuarios():
+def hello():
     return "Ola usuarios"
+
+
+@app.route("/dados")
+def getDados():
+    with open('dados.json', 'r') as json_file:
+        dadosSolar = json.load(json_file)
+    return dadosSolar
